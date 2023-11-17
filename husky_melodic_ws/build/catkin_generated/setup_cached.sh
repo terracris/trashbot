222074778f15ec642075880e808d50d25c4300d0
Cristobal Rincon Rogers <chrisrincon23@gmail.com>
2023-11-17 17:55:29 -0500

    packages added?

--- husky_melodic_ws/build/catkin_generated/setup_cached.sh
+++ husky_melodic_ws/build/catkin_generated/setup_cached.sh
@@ -7,8 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export LD_LIBRARY_PATH='/opt/ros/melodic/lib:/usr/local/cuda-10.2/lib64'
-export PKG_CONFIG_PATH='/opt/ros/melodic/lib/pkgconfig'
 export PWD='/home/trashbot/trashbot/husky_melodic_ws/build'
-export PYTHONPATH='/opt/ros/melodic/lib/python2.7/dist-packages'
-export ROS_PACKAGE_PATH='/home/trashbot/trashbot/husky_melodic_ws/src:/opt/ros/melodic/share'
