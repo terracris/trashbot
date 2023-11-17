8e608d7d5ff50c2c3da9ff23c857a060f26fac88
Cristobal Rincon Rogers <chrisrincon23@gmail.com>
2023-11-17 17:35:59 -0500

    adding workspace manually to avoid updates

---
+++ husky_melodic_ws/devel/share/husky_msgs/cmake/husky_msgs-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(husky_msgs_MSG_INCLUDE_DIRS "/home/trashbot/trashbot/husky_melodic_ws/src/husky/husky_msgs/msg")
+set(husky_msgs_MSG_DEPENDENCIES std_msgs)
