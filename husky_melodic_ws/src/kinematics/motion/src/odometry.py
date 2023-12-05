360035d3b2920f226ee8c151c5f2aa6f968f9819
Cristobal Rincon Rogers <chrisrincon23@gmail.com>
2023-12-05 17:49:37 -0500

    fixed some spacing issues. hopefully this compiles

--- husky_melodic_ws/src/kinematics/motion/src/odometry.py
+++ husky_melodic_ws/src/kinematics/motion/src/odometry.py
@@ -85,14 +85,14 @@ class Odometry:
         self.odometry_pub.publish(odometry_msg)
         self.odom_message_count += 1
 
-        def calculate_orientation(self, updated_theta):
-            # verify that you only need to update the z quaternion to determine the orientation.
-            quaternion = Quaternion
-            quaternion.z = updated_theta
+    def calculate_orientation(self, updated_theta):
+        # verify that you only need to update the z quaternion to determine the orientation.
+        quaternion = Quaternion
+        quaternion.z = updated_theta
 
-            return quaternion
+        return quaternion
 
-        def calculate_kinematics(self, left_wheel_velocity, right_wheel_velocity, delta_t):
+    def calculate_kinematics(self, left_wheel_velocity, right_wheel_velocity, delta_t):
         # Calculate linear and angular velocities of the Husky
         husky_linear_velocity = (left_wheel_velocity + right_wheel_velocity) / 2.0
         husky_angular_velocity = (right_wheel_velocity - left_wheel_velocity) / self.wheel_seperation
@@ -102,35 +102,34 @@ class Odometry:
         delta_y = 0
         delta_theta = 0
 
-        # check if velocities are equal (within some tolerance)
-        if self.have_same_sign(left_wheel_velocity, right_wheel_velocity) and self.in_tolerance(left_wheel_velocity, right_wheel_velocity):
-            delta_x = husky_linear_velocity * (cos(self.pth) * delta_t)
-            delta_y = husky_linear_velocity * (sin(self.pth) * delta_t)
+	    # check if velocities are equal (within some tolerance)
+	    if self.have_same_sign(left_wheel_velocity, right_wheel_velocity) and self.in_tolerance(left_wheel_velocity, right_wheel_velocity):
+    		delta_x = husky_linear_velocity * (cos(self.pth) * delta_t)
+			delta_y = husky_linear_velocity * (sin(self.pth) * delta_t)
 
-        # check if velocities are the opposite of each other (within some tolerance)
-        if not self.have_same_sign(left_wheel_velocity, right_wheel_velocity) and self.in_tolerance(left_wheel_velocity, right_wheel_velocity):
-            delta_theta = (2 * husky_linear_velocity * delta_t) / self.wheel_seperation
-        # else, they are different
-        else:
-            R = husky_linear_velocity / husky_angular_velocity
-            current_theta = self.pth
+         # check if velocities are the opposite of each other (within some tolerance)
+	    if not self.have_same_sign(left_wheel_velocity, right_wheel_velocity) and self.in_tolerance(left_wheel_velocity, right_wheel_velocity):
+	        delta_theta = (2 * husky_linear_velocity * delta_t) / self.wheel_seperation
+	    else:
+			R = husky_linear_velocity / husky_angular_velocity
+			current_theta = self.pth
 
-            delta_x = (-R * sin(current_theta)) + (R * sin(current_theta + (husky_angular_velocity * delta_t)))
-            delta_y = (R * cos(current_theta)) - (R * cos(current_theta + (husky_angular_velocity * delta_t)))
-            delta_theta = husky_angular_velocity * delta_t
+			delta_x = (-R * sin(current_theta)) + (R * sin(current_theta + (husky_angular_velocity * delta_t)))
+			delta_y = (R * cos(current_theta)) - (R * cos(current_theta + (husky_angular_velocity * delta_t)))
+			delta_theta = husky_angular_velocity * delta_t
 
-        return delta_x, delta_y, delta_theta, husky_linear_velocity, husky_angular_velocity
+		return delta_x, delta_y, delta_theta, husky_linear_velocity, husky_angular_velocity
 
-    def have_same_sign(self, left_wheel, right_wheel):
-        # if two numbers have the same sign, their multiplication will result in a positive number
-        return (left_wheel * right_wheel) > 0
+	def have_same_sign(self, left_wheel, right_wheel):
+	    # if two numbers have the same sign, their multiplication will result in a positive number
+		return (left_wheel * right_wheel) > 0
 
     def in_tolerance(self, left_wheel, right_wheel):
-        # determines if two wheels are within a given tolerance of each other
-        return abs(left_wheel - right_wheel) <= Odometry.VELOCITY_THRESHOLD
+	    # determines if two wheels are within a given tolerance of each other
+	    return abs(left_wheel - right_wheel) <= Odometry.VELOCITY_THRESHOLD
 
-    def run(self):
-        rospy.spin()
+	def run(self):
+	    rospy.spin()
 
 if __name__ == '__main__':
     odom = Odometry()
