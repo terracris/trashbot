b6f1e6d7548743b0cba10e824a368301eb09e075
Cristobal Rincon <chrisrincon23@gmail.com>
2023-12-09 22:39:42 -0500

    fixed spacing issues, changed the tolerance, and added Decimal classs to increase resolution of values read

--- husky_melodic_ws/src/kinematics/motion/src/odometry.py
+++ husky_melodic_ws/src/kinematics/motion/src/odometry.py
@@ -13,8 +13,9 @@ getcontext().prec = 10  # 10 decimal place precision
 
 class HuskyOdometry:
     # velocity threshold for determining equivalence. 
-    # currently set to 0.5 cm per second
-    VELOCITY_THRESHOLD = 0.005
+    #TODO find appropriate tolerance for wheel velocity to see difference --> considering wheels come with 16 decimal precision this could be good.
+    # currently set to 1 mm per second
+    VELOCITY_THRESHOLD = 0.001
 
     def __init__(self):
         rospy.init_node('a100_odometry', anonymous=True)
@@ -24,12 +25,12 @@ class HuskyOdometry:
         self.px = 0
         self.py = 0
         self.pth = 0
-        self.last_timestamp = rospy.Time.now()
+        self.last_timestamp = Decimal(rospy.Time.now())
         self.odom_message_count = 0
         
         # Husky A100 parameters [m]
-        self.wheel_seperation = 0.50 # number retrieved from manual on the A100
-        self.wheel_radius = 0.1143  # wheels are 9 inches in Diameter
+        self.wheel_seperation = Decimal(0.50) # number retrieved from manual on the A100
+        self.wheel_radius = Decimal(0.1143)  # wheels are 9 inches in Diameter
 
         # subscribers
         rospy.Subscriber('/joint_states', JointState, self.joint_states_callback)
@@ -41,15 +42,15 @@ class HuskyOdometry:
         current_timestamp = joint_states.header.stamp
 
         # Calculate time interval
-        delta_t = (current_timestamp - self.last_timestamp).to_sec()
+        delta_t = Decimal((current_timestamp - self.last_timestamp).to_sec())
 
         # Extract angular positions and velocities
-        left_motor_angular_velocity = joint_states.velocity[0]
-        right_motor_angular_velocity = joint_states.velocity[1]
+        left_motor_angular_velocity = Decimal(joint_states.velocity[0])
+        right_motor_angular_velocity = Decimal(joint_states.velocity[1])
 
         # Convert motor angular velocity to linear velocity for each wheel
-        left_wheel_linear_velocity = left_motor_angular_velocity * self.wheel_radius
-        right_wheel_linear_velocity = right_motor_angular_velocity * self.wheel_radius
+        left_wheel_linear_velocity = left_motor_angular_velocity * self.wheel_radius # resulting calculation should be of decimal object
+        right_wheel_linear_velocity = right_motor_angular_velocity * self.wheel_radius # resulting calculation should be of decimal object
         
         print("left wheel velocity: " + str(left_wheel_linear_velocity))
         print("right wheel velocity: " + str(right_wheel_linear_velocity))
@@ -103,46 +104,57 @@ class HuskyOdometry:
 
     def calculate_kinematics(self, left_wheel_velocity, right_wheel_velocity, delta_t):
         # Calculate linear and angular velocities of the Husky
+        # TODO verify if these variables are also Decimal types and what their precision is
         husky_linear_velocity = (left_wheel_velocity + right_wheel_velocity) / 2.0
         husky_angular_velocity = (right_wheel_velocity - left_wheel_velocity) / self.wheel_seperation
-        
+        print("husky linear velocity: " + str(husky_angular_velocity))
+        print("husky angular velocity: " + str(husky_angular_velocity))
+
         # define return variables
         delta_x = 0
         delta_y = 0
         delta_theta = 0
 
-	# check if velocities are equal (within some tolerance)
-	if self.have_same_sign(left_wheel_velocity, right_wheel_velocity) and self.in_tolerance(left_wheel_velocity, right_wheel_velocity):
-    	    delta_x = husky_linear_velocity * (cos(self.pth) * delta_t)
-    	    delta_y = husky_linear_velocity * (sin(self.pth) * delta_t)
+        # check if velocities are equal (within some tolerance)
+        if self.have_same_sign(left_wheel_velocity, right_wheel_velocity) and self.in_tolerance(left_wheel_velocity, right_wheel_velocity):
+            # TODO verify that these are also the decimal places we want
+            delta_x = husky_linear_velocity * (cos(self.pth) * delta_t)
+            delta_y = husky_linear_velocity * (sin(self.pth) * delta_t)
 
-	elif not self.have_same_sign(left_wheel_velocity, right_wheel_velocity) and self.in_tolerance(left_wheel_velocity, right_wheel_velocity):
-	    
-        # check if velocities are the opposite of each other (within some tolerance)
+            print("delta_x: " + str(delta_x))
+            print("delta_y: " + str(delta_y))
+
+        elif not self.have_same_sign(left_wheel_velocity, right_wheel_velocity) and self.in_tolerance(left_wheel_velocity, right_wheel_velocity):
+            # check if velocities are the opposite of each other (within some tolerance)
             delta_theta = (2 * husky_linear_velocity * delta_t) / self.wheel_seperation
-	else:
-	    R = husky_linear_velocity / husky_angular_velocity
-	    current_theta = self.pth
+            print("delta_theta is: " + str(delta_theta))
+        else:
+            R = husky_linear_velocity / husky_angular_velocity
+            current_theta = self.pth
 
-	    delta_x = (-R * sin(current_theta)) + (R * sin(current_theta + (husky_angular_velocity * delta_t)))
-	    delta_y = (R * cos(current_theta)) - (R * cos(current_theta + (husky_angular_velocity * delta_t)))
-	    delta_theta = husky_angular_velocity * delta_t
+            delta_x = (-R * sin(current_theta)) + (R * sin(current_theta + (husky_angular_velocity * delta_t)))
+            delta_y = (R * cos(current_theta)) - (R * cos(current_theta + (husky_angular_velocity * delta_t)))
+            delta_theta = husky_angular_velocity * delta_t
 
-	return delta_x, delta_y, delta_theta, husky_linear_velocity, husky_angular_velocity
+            print("delta_x: " + str(delta_x))
+            print("delta_y: " + str(delta_y))
+            print("delta_theta is: " + str(delta_theta))
+
+        return delta_x, delta_y, delta_theta, husky_linear_velocity, husky_angular_velocity
 
     def have_same_sign(self, left_wheel, right_wheel):
-	    # if two numbers have the same sign, their multiplication will result in a positive number
-	    # include zero in case both wheel velocities are zero and are accelerating
-            print("have same sign?: " + str(left_wheel * right_wheel >= 0))
-            return (left_wheel * right_wheel) >= 0
+        # if two numbers have the same sign, their multiplication will result in a positive number
+        # include zero in case both wheel velocities are zero and are accelerating
+        print("have same sign?: " + str(left_wheel * right_wheel >= 0))
+        return (left_wheel * right_wheel) >= 0
 
     def in_tolerance(self, left_wheel, right_wheel):
-	    # determines if two wheels are within a given tolerance of each other
-            print("in tolerance?: " + str(abs(left_wheel - right_wheel) <= HuskyOdometry.VELOCITY_THRESHOLD))
-	    return abs(left_wheel - right_wheel) <= HuskyOdometry.VELOCITY_THRESHOLD
+        # determines if two wheels are within a given tolerance of each other
+        print("in tolerance?: " + str(abs(left_wheel - right_wheel) <= HuskyOdometry.VELOCITY_THRESHOLD))
+        return abs(left_wheel - right_wheel) <= HuskyOdometry.VELOCITY_THRESHOLD
 
     def run(self):
-	print("running")    
+        print("running")
         rospy.spin()
 
 if __name__ == '__main__':
