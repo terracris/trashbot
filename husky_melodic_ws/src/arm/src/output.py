e25fafbfe25d92d37af3b6c1ef97464eba75ae00
Cristobal Rincon Rogers <chrisrincon23@gmail.com>
2024-03-21 15:14:22 -0400

    i honestly don't know what changes we made, but we need a save point!

--- husky_melodic_ws/src/arm/src/output.py
+++ husky_melodic_ws/src/arm/src/output.py
@@ -3,17 +3,20 @@ import Jetson.GPIO as GPIO
 # set the GPIO mode to BCM
 GPIO.setmode(GPIO.BOARD)
 
-pin_1 = 37
-
-state = GPIO.LOW
+pin_1 = 13
+pin_2 = 21
+state = GPIO.HIGH
 
 # set up GPIO pin 8, 10 as output
 GPIO.setup(pin_1, GPIO.OUT)
+GPIO.setup(pin_2, GPIO.OUT)
 
 GPIO.output(pin_1, state)
+GPIO.output(pin_2, state)
 
 try:
     while True:
         print(pin_1, ": ", state)
+        print(pin_2, ": ", state)
 except KeyboardInterrupt:
     GPIO.cleanup()
