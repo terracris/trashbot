aad31d979b38a41f1e3adf5ae8411bf95c1a5f7c
Cristobal Rincon Rogers <chrisrincon23@gmail.com>
2024-03-25 16:32:09 -0400

    added ros installation to config file

--- husky_melodic_ws/deps.sh
+++ husky_melodic_ws/deps.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+sudo apt install curl # if you haven't already installed curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+
+sudo apt update
+sudo apt install ros-melodic-desktop-full
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt install python-rosdep
+
+sudo rosdep init
+rosdep update
+
 rosdep install --from-paths src --ignore-src -r -y
 
 sudo apt-get install ros-melodic-velodyne-gazebo-plugins -y
